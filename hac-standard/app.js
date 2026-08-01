151f088c2ab68cef1461c4a5622d41e1c32bb379
mikefreiman <mikefreiman.now@gmail.com>
2026-08-01 17:51:47 -0500

    Blur and Icon Crush

--- hac-standard/app.js
+++ hac-standard/app.js
@@ -2,20 +2,6 @@ const openButton = document.getElementById("open-sidebar-button");
 const navmenu = document.getElementById("navmenu");
 const overlay = document.getElementById("overlay");
 
-// const media = window.matchMedia("(width < 910px)");
-// media.addEventListener("change", (e) => updatenavmenu(e));
-
-// function updatenavmenu(e) {
-//   const isMobile = e.matches;
-//   if (isMobile) {
-//     // on mobile, inert should reflect whether the menu is currently closed
-//     navmenu.toggleAttribute("inert", !isMenuOpen);
-//   } else {
-//     // desktop: always interactive
-//     navmenu.removeAttribute("inert");
-//   }
-// }
-
 // Get the button element by its ID
 const navToggle = document.getElementById("navicon");
 const nav = document.querySelector("nav");
@@ -35,25 +21,3 @@ function closeMenu() {
   });
 }
 closeMenu();
-
-// function openSidebar() {
-//   navmenu.classList.add("show");
-//   openButton.setAttribute("aria-expanded", "true");
-//   navmenu.removeAttribute("inert");
-// }
-
-// function closeSidebar() {
-//   navmenu.classList.remove("show");
-//   openButton.setAttribute("aria-expanded", "false");
-//   navmenu.setAttribute("inert", "");
-// }
-
-// For Bookmark Links
-// const navLinks = document.querySelectorAll('nav a')
-// navLinks.forEach(link => {
-//   link.addEventListener('click', () => {
-//     closeSidebar()
-//   })
-// })
-
-updatenavmenu(media);
